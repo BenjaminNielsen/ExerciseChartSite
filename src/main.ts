2c81f5f74e5a84d891f2c33ad10d7ada72873149
benjamin_nielsen <ben.a.nielsen@gmail.com>
2020-09-14 15:56:44 -0400

    Trying to fix some amplify mumbo jumbo

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,15 @@ import {AppModule} from './app/app.module'
 import {environment} from './environments/environment'
 
 import Amplify from 'aws-amplify'
-const awsExports = require('./aws-exports').default
+// const awsExports = require('./aws-exports').default
 
-Amplify.configure(awsExports)
+Amplify.configure({
+  aws_project_region: 'us-east-2',
+  aws_appsync_graphqlEndpoint: 'https://fl56rby2jrbv7a6pd52stzpd44.appsync-api.us-east-2.amazonaws.com/graphql',
+  aws_appsync_region: 'us-east-2',
+  aws_appsync_authenticationType: 'API_KEY',
+  aws_appsync_apiKey: 'da2-hn6gsvoqzzfj5gugnt5f4s2uhm'
+})
 // TODO The code above imports the entire Amplify library. You can use separate imports like import Auth from '@aws-amplify/auth' to reduce the final bundle size
 
 if (environment.production) {
